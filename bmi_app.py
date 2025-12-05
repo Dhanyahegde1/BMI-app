6a83c17abada4515a1e7de3c4f3498cff3eade39
Dhanya Hegde <155796110+Dhanyahegde1@users.noreply.github.com>
2025-12-05 12:49:15 +0530

    Update bmi_app.py

--- bmi_app.py
+++ bmi_app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1nV81wZ1XtsaRpwLy-a9T1svLSF7SvNVn
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
